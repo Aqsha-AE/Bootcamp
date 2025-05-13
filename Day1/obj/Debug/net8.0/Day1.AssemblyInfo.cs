df908334b69ac20832c43522a8d9cabdde090f90
Aqsha-AE <aqshaananda2@gmail.com>
2025-05-13 11:38:43 +0700

    feat : change solution

--- Day1/obj/Debug/net8.0/Day1.AssemblyInfo.cs
+++ Day1/obj/Debug/net8.0/Day1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68d9092ce5fe3dd7b4dc15f00f6e3e8b96853cfb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68df5c89b98889b772d324fcb8626421feff3501")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
