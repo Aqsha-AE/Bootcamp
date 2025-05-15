52e483f0620016ead658e77769ce2975191e6fb6
Aqsha-AE <aqshaananda2@gmail.com>
2025-05-15 14:58:13 +0700

    feat : add day 3

--- Day3/Inheritance/Program.cs
+++ Day3/Inheritance/Program.cs
@@ -1,15 +1,17 @@
-﻿class Room
+﻿public class Room
 {
     public string Name;
+    // nilai default dari field adalah null
+    //public string Name = "tes";
 }
 
-class Bedroom : Room
+public class Bedroom : Room
 {
     public string BedCover;
     public int BedSize;
 }
 
-class RumahSakit : Room
+public class RumahSakit : Room
 {
 
     public string JenisKamar;
@@ -17,7 +19,7 @@ class RumahSakit : Room
     public int JumlahKasur;
 }
 
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
@@ -26,5 +28,36 @@ class Program
 
         RumahSakit hermina = new RumahSakit { Name = "pakboy", JenisKamar = "VIP", NomorKamar = 101, JumlahKasur = 2 };
         Console.WriteLine($"Name: {hermina.Name} dirawat di rumah sakit dengan jenis kamar {hermina.JenisKamar} dengan nomor kamar {hermina.NomorKamar} dan jumlah kasur {hermina.JumlahKasur}");
+
+        // upcasting
+        Bedroom kiki = new Bedroom { Name = "Kiki" };
+        Room c = kiki; // upcasting
+        Console.WriteLine($"Name: {c.Name}"); // Name: Kiki
+
+        /*
+        // downcasting Invalid 
+        Room room_1 = new Room();
+        Bedroom bedroom_1 = (Bedroom)room_1; 
+        Console.WriteLine($"Name: {bedroom_1.Name}"); // InvalidCastException
+        */
+
+        // downcasting
+        Room room_2 = new Bedroom { Name = "Eka" };
+        Bedroom bedroom_2 = (Bedroom)room_2;
+        Console.WriteLine($"Name: {bedroom_2.Name}"); // Name: Eka
+
+        // safer downcasting 
+        Room room_3 = new Bedroom { Name = "Jaya" };
+        Bedroom bedroom_3 = room_3 as Bedroom;
+        Console.WriteLine($"Name: {bedroom_3.Name}"); // Name: Jaya
+
+        // safer downcasting
+        Room z = new Bedroom ();
+        Bedroom u = z as Bedroom;
+        if (u != null) {
+            Console.WriteLine(z.Name);
+        } else {
+            Console.WriteLine("The Cast is not valid.");
+        }
     }
 }
