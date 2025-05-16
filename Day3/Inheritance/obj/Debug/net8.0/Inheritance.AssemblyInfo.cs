7268dcfdbd5b591ab89294e0a80efffb2b965f09
Aqsha-AE <aqshaananda2@gmail.com>
2025-05-16 10:02:24 +0700

    feat : add day 4

--- Day3/Inheritance/obj/Debug/net8.0/Inheritance.AssemblyInfo.cs
+++ Day3/Inheritance/obj/Debug/net8.0/Inheritance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfd5b2935caa96f1d0806ad95ba08584f70d8b5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e034b9ad34e034bd6dcfd473305b54bfb88a05f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Day4/testing/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Tesat;
+
+public struct Demo
+{
+    public string c = "huru";
+    public string d;
+    public Demo() => d = "hara";
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Demo jakarta = new Demo();
+        Demo surabaya = default;    
+        Console.WriteLine(jakarta.c + jakarta.d);
+        Console.WriteLine(surabaya.c + surabaya.d);
+    }
+}
